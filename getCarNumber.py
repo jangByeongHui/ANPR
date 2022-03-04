85f89a54e7bd595230c22c8c81767e235feac75e
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-03-04 13:39:40 +0900

    change FONt

--- getCarNumber.py
+++ getCarNumber.py
@@ -1,8 +1,8 @@
 import torch
 import cv2
-# from KaKaoOCR import kakao_ocr
+from KaKaoOCR import kakao_ocr
 # from tesseract_OCR import Image2String
-from Easy_OCR import EasyOCR
+import easyocr
 from PIL import Image,ImageDraw,ImageFont
 import matplotlib.pyplot as plt
 import re
@@ -19,16 +19,22 @@ COLORS = [(218,229,0),(173,0,186),(113,206,0)]
 re_car_num = re.compile('/^\D{2}\d{2}\D\d{4}$/')
 
 #yolov5 모델 로드
-model = torch.hub.load('yolov5','custom',path='ANPR_V1.pt',source='local') #yolov5 모델 load
+model = torch.hub.load('yolov5','custom',path='ANPR_V2.pt',source='local') #yolov5 모델 load
 # model.classes=[2] # 차량만 검출
 # model.conf=0.5
+reader = easyocr.Reader(['ko'],gpu=True)
+def EasyOCR(img):
+    result = reader.readtext(img)
+    text=""
+    for sentence in result:
+        text += sentence[-2]
+    return text
 
 def detect(img):
-    img = TransHough(img)
     detects = model(img,size=640)
     
     crop_image=[]
-    font = ImageFont.truetype("fonts/DejaVuSansMono.ttf",20)
+    font = ImageFont.truetype("/usr/share/fonts/nanum/NanumBrush.ttf",20)
     
     for num,det in enumerate(detects.pandas().xyxy[0].values.tolist()):
         #Detect 결과
@@ -40,12 +46,13 @@ def detect(img):
         cls=int(cls)
         
         if cls == 1:
-            
             car_plate = img[y1:y2, x1:x2]
-            car_plate = cv2.cvtColor(car_plate,cv2.COLOR_BGR2GRAY)
             crop_image.append(car_plate)
-            CarNumber = EasyOCR(img[y1:y2,x1:x2])
+            # car_plate=TransHough(car_plate)
+            CarNumber = EasyOCR(car_plate)
+
             if CarNumber !="":
+                print("OCR_RESULT",CarNumber)
                 img=Image.fromarray(img)
                 draw = ImageDraw.Draw(img)
                 draw.rectangle(((x1, y1), (x2, y2)), outline=COLORS[cls], width=2)  # bounding box
@@ -68,23 +75,30 @@ def detect(img):
 
 
 if __name__ == '__main__':
-    video_path="data/test2.mp4"
+    video_path="data/test3.mp4"
     cap = cv2.VideoCapture(video_path)
+    w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('./runs/output.mp4', fourcc, fps, (w, h))
+
     count=0
     while True:
         ret, frame = cap.read()
 
         if ret:
             view_img,crop_image = detect(frame)
-            for num,i in enumerate(crop_image):
-                cv2.imwrite(f'runs/crop/{count}_{num}.jpg',i)
-            #cv2.imshow("View_img",view_img)
-            # plt.show()
+            # for num,i in enumerate(crop_image):
+            #     cv2.imwrite(f'runs/crop/{count}_{num}.jpg',i)
+            # cv2.imshow("View_img",view_img)
+            out.write(view_img)
             cv2.imwrite(f'runs/view/{count}.jpg',view_img)
             count+=1
-            # key = cv2.waitKey(1)
+            # key = cv2.waitKey(15)
             # if key ==27:
             #     break
         else:
             cap.release()
+            out.release()
             break
